917549687745e701b589f5b9f23642f2cf2ff128
Kazuki Nakano <kakky0312@gmail.com>
2014-05-22 16:34:06 +0900

    Update default.rb

--- site-cookbooks/change-apt-server/recipes/default.rb
+++ site-cookbooks/change-apt-server/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright 2014, YOUR_COMPANY_NAME
 #
-# All rights reserved - Do Not Redistribute
+# All rights reserved - Do Not Redistribute 
 #
 cookbook_file "/etc/apt/sources.list" do
     source "sources.list"
